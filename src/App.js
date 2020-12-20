f4c1ec2fa661224fd33616f81e719b1a466fa2be
Kay <joe_kayu@yahoo.com>
2020-12-20 16:29:20 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+
+function App() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const openSidebar = () => {
+    setSidebarOpen(true)
+  }
+  const closeSidebar = () => {
+    setSidebarOpen(false)
+  }
+
+  return (
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
+      <Sidebar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
+
+      <h1>React Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
