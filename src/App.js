d5af6ae65db07b591a12ae0f8d43cdb246e72391
Kay <joe_kayu@yahoo.com>
2020-12-20 19:52:37 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import { useState } from 'react';
-import Main from './components/main/Main';
-import Navbar from './components/navbar/Navbar';
-import Sidebar from './components/sidebar/Sidebar';
+import React from 'react';
 
-function App() {
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
 
-  const [sidebarOpen, setSidebarOpen] = useState(false)
+import image from './images/image.png';
 
-  const openSidebar = () => {
-    setSidebarOpen(true)
-  }
-  const closeSidebar = () => {
-    setSidebarOpen(false)
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
   }
 
-  return (
-    <div className="container">
-      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar}/>
-      <Main />
+  async componentDidMount() {
+    const data = await fetchData();
 
-      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar}/>
+    this.setState({ data });
+  }
 
-    </div>
-  );
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
