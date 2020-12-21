0b66e6fb2d4daa6245c1da8bb7965c7c448d9a30
Kay <joe_kayu@yahoo.com>
2020-12-21 13:35:59 +0100

    ml

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-
-// core components
-import Admin from "layouts/Admin.js";
-import RTL from "layouts/RTL.js";
-
-import "assets/css/material-dashboard-react.css?v=1.9.0";
-
-const hist = createBrowserHistory();
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
+import "./i18n";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>,
-  document.getElementById("root")
-);
+  <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
+    <App />
+  </BrowserRouter>
+, document.getElementById('root'));
+
+serviceWorker.unregister();
