2f225830fd60614730521940a01b05de491637dc
Kay <joe_kayu@yahoo.com>
2020-12-21 11:26:13 +0100

    material-dashboard

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,80 +1,163 @@
+/*eslint-disable*/
 import React from "react";
-import logo from "../../assets/logo.png";
-import './Sidebar.css'
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Icon from "@material-ui/core/Icon";
+// core components
+import AdminNavbarLinks from "components/Navbars/AdminNavbarLinks.js";
+import RTLNavbarLinks from "components/Navbars/RTLNavbarLinks.js";
 
-const Sidebar = ({ sidebarOpen, closeSidebar}) => {
-  return (
-    <div className={sidebarOpen ? "sidebar-responsive" : ""} id="sidebar">
-      <div className="sidebar__title">
-        <div className="sidebar__img">
-          <img src={logo} alt="logo" />
-          <h1>Codersbite</h1>
+import styles from "assets/jss/material-dashboard-react/components/sidebarStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Sidebar(props) {
+  const classes = useStyles();
+  // verifies if routeName is the one active (in browser input)
+  function activeRoute(routeName) {
+    return window.location.href.indexOf(routeName) > -1 ? true : false;
+  }
+  const { color, logo, image, logoText, routes } = props;
+  var links = (
+    <List className={classes.list}>
+      {routes.map((prop, key) => {
+        var activePro = " ";
+        var listItemClasses;
+        if (prop.path === "/upgrade-to-pro") {
+          activePro = classes.activePro + " ";
+          listItemClasses = classNames({
+            [" " + classes[color]]: true
+          });
+        } else {
+          listItemClasses = classNames({
+            [" " + classes[color]]: activeRoute(prop.layout + prop.path)
+          });
+        }
+        const whiteFontClasses = classNames({
+          [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+        });
+        return (
+          <NavLink
+            to={prop.layout + prop.path}
+            className={activePro + classes.item}
+            activeClassName="active"
+            key={key}
+          >
+            <ListItem button className={classes.itemLink + listItemClasses}>
+              {typeof prop.icon === "string" ? (
+                <Icon
+                  className={classNames(classes.itemIcon, whiteFontClasses, {
+                    [classes.itemIconRTL]: props.rtlActive
+                  })}
+                >
+                  {prop.icon}
+                </Icon>
+              ) : (
+                <prop.icon
+                  className={classNames(classes.itemIcon, whiteFontClasses, {
+                    [classes.itemIconRTL]: props.rtlActive
+                  })}
+                />
+              )}
+              <ListItemText
+                primary={props.rtlActive ? prop.rtlName : prop.name}
+                className={classNames(classes.itemText, whiteFontClasses, {
+                  [classes.itemTextRTL]: props.rtlActive
+                })}
+                disableTypography={true}
+              />
+            </ListItem>
+          </NavLink>
+        );
+      })}
+    </List>
+  );
+  var brand = (
+    <div className={classes.logo}>
+      <a
+        href="https://www.creative-tim.com?ref=mdr-sidebar"
+        className={classNames(classes.logoLink, {
+          [classes.logoLinkRTL]: props.rtlActive
+        })}
+        target="_blank"
+      >
+        <div className={classes.logoImage}>
+          <img src={logo} alt="logo" className={classes.img} />
         </div>
-        <i
-          className="fa fa-times"
-          id="sidebarIcon"
-          onClick={() => closeSidebar()}
-        ></i>
-      </div>
-      <div className="sidebar__menu">
-        <div className="sidebar__link active_menu_link">
-          <i className="fa fa-home"></i>
-          <a href="#">Dashboard</a>
-        </div>
-        <h2>MNG</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-user-secret"></i>
-          <a href="#">Admin Management</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-building-o"></i>
-          <a href="#">Company Management</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-wrench"></i>
-          <a href="#">Employee Management</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-archive"></i>
-          <a href="#">Warehouse</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-handshake-o"></i>
-          <a href="#">Contracts</a>
-        </div>
-        <h2>LEAVE</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-question"></i>
-          <a href="#">Requests</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-sign-out"></i>
-          <a href="#">Leave policy</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-calendar-check-o"></i>
-          <a href="#">Special days</a>
-        </div>
-        <div className="sidebar__link">
-          <i className="fa fa-files-o"></i>
-          <a href="#">Apply for leave</a>
-        </div>
-        <h2>PAYROLL</h2>
-        <div className="sidebar__link">
-          <i className="fa fa-money"></i>
-          <a href="#">Payroll</a>
-        </div>     
-        <div className="sidebar__link">
-          <i className="fa fa-briefcase"></i>
-          <a href="#">paygrade</a>
-        </div>
-        <div className="sidebar__logout">
-          <i className="fa fa-power-off"></i>
-          <a href="#">Log out</a>
-        </div>
-      </div>
+        {logoText}
+      </a>
     </div>
   );
-};
+  return (
+    <div>
+      <Hidden mdUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor={props.rtlActive ? "left" : "right"}
+          open={props.open}
+          classes={{
+            paper: classNames(classes.drawerPaper, {
+              [classes.drawerPaperRTL]: props.rtlActive
+            })
+          }}
+          onClose={props.handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>
+            {props.rtlActive ? <RTLNavbarLinks /> : <AdminNavbarLinks />}
+            {links}
+          </div>
+          {image !== undefined ? (
+            <div
+              className={classes.background}
+              style={{ backgroundImage: "url(" + image + ")" }}
+            />
+          ) : null}
+        </Drawer>
+      </Hidden>
+      <Hidden smDown implementation="css">
+        <Drawer
+          anchor={props.rtlActive ? "right" : "left"}
+          variant="permanent"
+          open
+          classes={{
+            paper: classNames(classes.drawerPaper, {
+              [classes.drawerPaperRTL]: props.rtlActive
+            })
+          }}
+        >
+          {brand}
+          <div className={classes.sidebarWrapper}>{links}</div>
+          {image !== undefined ? (
+            <div
+              className={classes.background}
+              style={{ backgroundImage: "url(" + image + ")" }}
+            />
+          ) : null}
+        </Drawer>
+      </Hidden>
+    </div>
+  );
+}
 
-export default Sidebar;
+Sidebar.propTypes = {
+  rtlActive: PropTypes.bool,
+  handleDrawerToggle: PropTypes.func,
+  bgColor: PropTypes.oneOf(["purple", "blue", "green", "orange", "red"]),
+  logo: PropTypes.string,
+  image: PropTypes.string,
+  logoText: PropTypes.string,
+  routes: PropTypes.arrayOf(PropTypes.object),
+  open: PropTypes.bool
+};
