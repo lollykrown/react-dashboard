8161b4d4ebec5bc11595b5a94a38d638bbf5b1f3
Kay <joe_kayu@yahoo.com>
2020-12-21 14:28:09 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./i18n";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter basename="/demo/corona-react-free/template/demo_1/preview">
+  <BrowserRouter basename="/react-dashboard/preview">
     <App />
   </BrowserRouter>
 , document.getElementById('root'));
