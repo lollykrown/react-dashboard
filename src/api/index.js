d6211f8323be9804bddc04dfcbbddc6b5ca97474
Kay <joe_kayu@yahoo.com>
2020-12-21 10:36:56 +0100

    .

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
+// const world = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php";
+// const byCountry = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php";
+// const affectedCountriesList = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/affected.php";
+
 export const fetchData = async (country) => {
   let changeableUrl = url;
 
@@ -18,15 +22,6 @@ export const fetchData = async (country) => {
   }
 };
 
-// export const fetchDailyData = async () => {
-//   try {
-//     const { data } = await axios.get(`${url}/daily`);
-
-//     return data.map(({ confirmed, deaths, reportDate: date }) => ({ confirmed: confirmed.total, deaths: deaths.total, date }));
-//   } catch (error) {
-//     return error;
-//   }
-// };
 
 // Instead of Global, it fetches the daily data for the US
 export const fetchDailyData = async () => {
@@ -39,6 +34,7 @@ export const fetchDailyData = async () => {
     }
   };
 
+  
 export const fetchCountries = async () => {
   try {
     const { data: { countries } } = await axios.get(`${url}/countries`);
@@ -48,3 +44,33 @@ export const fetchCountries = async () => {
     return error;
   }
 };
+
+// export const fetchDailyData = async () => {
+//   try {
+//     const { data } = await axios.get(world,  {
+//       headers: {
+//         "x-rapidapi-key": "483dc3069bmsh0df5c38d47dc0bep117fdbjsn3bd00ed53303",
+//         "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com"
+//       }
+//     })
+// console.log('data', data.total_cases)
+//     return data.map(({ total_cases, total_recovered, total_deaths, statistics_taken_at: date }) => ({ confirmed: total_cases, recovered:total_recovered, death:total_deaths, date }));
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
+// export const fetchCountries = async () => {
+//   try {
+//     const { data: { countries_stat } } = await axios.get(byCountry,{
+//             headers: {
+//               "x-rapidapi-key": "483dc3069bmsh0df5c38d47dc0bep117fdbjsn3bd00ed53303",
+//               "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com"
+//             }
+//           })
+
+//     return countries_stat.map((country) => country.country_name).sort();
+//   } catch (error) {
+//     return error;
+//   }
+// };
